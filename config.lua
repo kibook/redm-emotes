437323b7c7fb8e05c28334237c20b7a7fae6c3fe
kibukj <kibuzhj@gmail.com>
2021-05-20 21:02:45 -0400

    Add emotes

--- config.lua
+++ config.lua
@@ -26,6 +26,87 @@ Config.Emotes = {
 			flag = 1
 		}
 	},
+	["dogbarkground"] = {
+		type = "solo",
+		name = "Dog: Bark on Ground",
+		animation = {
+			dict = "amb_creature_mammal@world_dog_barking_ground@base",
+			name = "base",
+			flag = 1
+		}
+	},
+	["dogbarkup"] = {
+		type = "solo",
+		name = "Dog: Bark Up",
+		animation = {
+			dict = "amb_creature_mammal@world_dog_barking_up@base",
+			name = "base",
+			flag = 1
+		},
+	},
+	["dogrest"] = {
+		type = "solo",
+		name = "Dog: Rest",
+		animation = {
+			dict = "amb_creature_mammal@world_dog_resting@base",
+			name = "base",
+			flag = 1
+		}
+	},
+	["dogroll"] = {
+		type = "solo",
+		name = "Dog: Roll",
+		animation = {
+			dict = "amb_creature_mammal@world_dog_roll_ground@base",
+			name = "base",
+			flag = 1
+		}
+	},
+	["dogsit"] = {
+		type = "solo",
+		name = "Dog: Sit",
+		animation = {
+			dict = "amb_creature_mammal@world_dog_sitting@base",
+			name = "base",
+			flag = 1
+		}
+	},
+	["dogsleep"] = {
+		type = "solo",
+		name = "Dog: Sleep",
+		animation = {
+			dict = "amb_creature_mammal@world_dog_sleeping@base",
+			name = "base",
+			flag = 1
+		}
+	},
+	["dogwagtail"] = {
+		type = "solo",
+		name = "Dog: Wag Tail",
+		animation = {
+			dict = "creatures_mammal@dog_pers@happy@idle",
+			name = "idle",
+			flag = 1
+		}
+	},
+	["handsup"] = {
+		type = "solo",
+		name = "Hands Up",
+		animation = {
+			dict = "script_proc@robberies@homestead@lonnies_shack@deception",
+			name = "hands_up_loop",
+			flag = 25
+		}
+	},
+	["pet1"] = {
+		type = "solo",
+		name = "Pet",
+		animation = {
+			dict = "mech_animal_interaction@dog@patting@1h",
+			name = "base",
+			flag = 1
+		}
+	},
 
 	-- Shared emotes
 	["dance"] = {
@@ -45,6 +126,40 @@ Config.Emotes = {
 			}
 		}
 	},
+	["pet"] = {
+		type = "shared",
+		name = "Pet Dog (Standing)",
+		animation = {
+			dict = "mech_animal_interaction@dog@patting@1h",
+			name = "base",
+			flag = 1
+		},
+		partner = {
+			offset = vector4(0, 0.85, -0.6, 180),
+			animation = {
+				dict = "creatures_mammal@dog_pers@happy@idle",
+				name = "idle",
+				flag = 1
+			}
+		}
+	},
+	["pet2"] = {
+		type = "shared",
+		name = "Pet Dog (Sitting)",
+		animation = {
+			dict = "mech_animal_interaction@dog@patting@1h",
+			name = "base",
+			flag = 1
+		},
+		partner = {
+			offset = vector4(0, 0.7, -0.6, 180),
+			animation = {
+				dict = "amb_creature_mammal@world_dog_sitting@base",
+				name = "base",
+				flag = 1
+			}
+		}
+	},
 
 	-- Prop emotes
 	["basket"] = {
@@ -60,6 +175,31 @@ Config.Emotes = {
 			dict = "amb_wander@code_human_basket_wander@female_a@base",
 			name = "base",
 			flag = 25
+		}
+	},
+	["dogbone"] = {
+		type = "prop",
+		name = "Dog Bone",
+		prop = {
+			model = "p_dogbone01x",
+			bone = "skel_head",
+			position = vector3(0.18, 0.02, 0.0),
+			rotation = vector3(0.0, 90.0, 0.0)
+		}
+	},
+	["dogchewbone"] = {
+		type = "prop",
+		name = "Chew on Bone",
+		prop = {
+			model = "p_dogbone01x",
+			bone = "skel_head",
+			position = vector3(0.18, 0.02, 0.0),
+			rotation = vector3(0.0, 90.0, 0.0)
+		},
+		animation = {
+			dict = "amb_creature_mammal@prop_dog_eat_bone@base",
+			name = "base",
+			flag = 1
 		}
 	},
 	["parasol"] = {
